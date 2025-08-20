85d246b024da6854a0e9b9cf3dd23d5e4d37d3c3
magumanakayama <domoido@icloud.com>
2025-08-21 00:09:15 +0900

    マージン調整、検索時の戻るボタン追加

--- frontend/src/components/search/SearchBox.jsx
+++ frontend/src/components/search/SearchBox.jsx
@@ -2,7 +2,7 @@ import { TextField, Button, CircularProgress, Stack } from '@mui/material';
 
 const SearchBox = ({ query, setQuery, handleSearch, loading }) => {
   return (
-    <Stack direction="column" spacing={1}>
+    <Stack direction="column" sx={{ m: 2 }} spacing={1}>
       <Stack direction="row" spacing={1}>
         {Object.keys(query).map(key => (
           <TextField
@@ -16,6 +16,7 @@ const SearchBox = ({ query, setQuery, handleSearch, loading }) => {
       </Stack>
       {/* <Button variant="contained" onClick={() => setQuery({ ...query, title: '告白', author: '湊かなえ' })}>デバックプリセット</Button> */}
       <Stack direction="row" justifyContent="flex-end" spacing={1}>
+        <Button variant="outlined" onClick={() => window.history.back()} >戻る</Button>
         <Button variant="contained" onClick={() => setQuery({ ...query, author: '湊かなえ' })}>デバックプリセット2</Button>
         <Button variant="contained" onClick={handleSearch} sx={{ display: loading ? 'none' : 'block' }}>検索</Button>
         <CircularProgress sx={{ display: loading ? 'block' : 'none', ml: 1 }} />
