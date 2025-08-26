2236b4207cf70dd9f5fa532b95ce8d0780496346
magumanakayama <domoido@icloud.com>
2025-08-26 23:28:33 +0900

    お試し2

--- frontend/src/components/SpeedDialButtons.jsx
+++ frontend/src/components/SpeedDialButtons.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Box, SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
+import { Box, Button, SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
 import { Search, BarChart, Add } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../constant';
@@ -20,7 +20,7 @@ const SpeedDialButtons = ({ setOpen }) => {
 
   return (
     <Box sx={{ position: 'fixed', bottom: 16, right: 16 }}>
-      <SpeedDial ariaLabel="SpeedDial" open={dialOpen} onOpen={() => setDialOpen(true)} onClose={() => setDialOpen(false)} icon={<SpeedDialIcon />}>
+      <SpeedDial ariaLabel="SpeedDial" open={dialOpen} onClick={() => setDialOpen(!dialOpen)} icon={<SpeedDialIcon />}>
         {actions.map((action) => <SpeedDialAction key={action.name} icon={action.icon} onClick={action.onClick} />)}
       </SpeedDial>
     </Box>
