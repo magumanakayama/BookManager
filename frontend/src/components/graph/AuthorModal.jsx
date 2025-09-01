a54ea55e46fc07a63e8bb870a4b539702f538af8
magumanakayama <domoido@icloud.com>
2025-09-01 23:43:17 +0900

    useLayoutEffect実装

--- frontend/src/components/graph/AuthorModal.jsx
+++ frontend/src/components/graph/AuthorModal.jsx
@@ -1,11 +1,11 @@
 import { Box, Button, Modal, List, ListItem, ListItemText } from '@mui/material';
 import listFormatter from './listFormatter';
-import { useRef } from 'react';
+import { useRef, useLayoutEffect, useState } from 'react';
 
 
 const AuthorModal = ({ open, setOpen, blurProps, selectedAuthor, setSelectedAuthor, bookInfo }) => {
   const buttonRef = useRef(null);
-  console.log(buttonRef);
+  const [listMaxHeight, setListMaxHeight] = useState('100%');
   const modalStyle = {
     position: 'absolute',
     top: 0,
@@ -23,11 +23,23 @@ const AuthorModal = ({ open, setOpen, blurProps, selectedAuthor, setSelectedAuth
   const { authorFormattedBookList } = listFormatter(bookInfo);
   const selectedAuthorBooks = authorFormattedBookList.filter(book => book.author.replace(/\s+/g, '') === selectedAuthor);
 
+
+  // useLayoutEffect実行時にはrefがセットされていない可能性があるため、setTimeoutで遅延させrefセットを意図的に待つ(LayoutEffectとrefのどっちが早いかは明確にはわからないため暫定対応)
+  useLayoutEffect(() => {
+    if (open) {
+      setTimeout(() => {
+        if (buttonRef.current) {
+          setListMaxHeight(`calc(100% - ${buttonRef.current.offsetHeight}px)`);
+        }
+      }, 0);
+    }
+  }, [open]);
+
   return (
     <Modal open={open} onClose={handleClose}>
       <Box sx={modalStyle}>
         <Button ref={buttonRef} onClick={handleClose}>Close</Button>
-        <Box sx={{ maxHeight: `calc(100% - ${buttonRef.current.offsetHeight}px)`, overflowY: 'auto' }}>
+        <Box sx={{ maxHeight: listMaxHeight, overflowY: 'auto' }}>
           <List>
             {selectedAuthorBooks.map((book) => (
               <ListItem key={book.isbn} sx={{ overflow: 'hidden' }}>
