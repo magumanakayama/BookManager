38d83d743c57e28fc7a29d129c5bdbfb80d7bf7b
magumanakayama <domoido@icloud.com>
2025-09-05 00:15:14 +0900

    y軸目盛バグ修正

--- frontend/src/components/graph/MonthlyGraph.jsx
+++ frontend/src/components/graph/MonthlyGraph.jsx
@@ -9,7 +9,7 @@ const MonthlyGraph = ({ monthlyList }) => {
         xAxis={[{ dataKey: 'month', scaleType: 'band', tickSize: 0 }]} // datakeyがx軸のラベルになる
         series={[{ dataKey: 'count', label: '月別の本の数' }]} // datakeyがy軸の値になる
         barLabel={(item) => item.value > 0 ? item.value?.toString() : null} // 0のラベルを非表示に
-        yAxis={[{ valueFormatter: v => Math.floor(v).toString() > 0 ? Math.floor(v).toString() : null }, { tickSize: 0 }]} // y軸を整数表示に
+        yAxis={[{ valueFormatter: v => Math.floor(v).toString() > 0 ? Math.floor(v).toString() : null, tickSize: 0 }]} // y軸を整数表示に
         height={320}
         width={360}
         margin={{ left: 0 }}
