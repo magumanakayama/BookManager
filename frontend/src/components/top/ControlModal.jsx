07bbac6833ac0813614c29692f3206837c3304b5
magumanakayama <domoido@icloud.com>
2025-09-14 15:58:47 +0900

    モーダルアニメーション削除

--- frontend/src/components/top/ControlModal.jsx
+++ frontend/src/components/top/ControlModal.jsx
@@ -35,26 +35,19 @@ const ControlModal = ({ modalMode, open, setOpen, bookInfo, setBookInfo = () => 
 
 
   return (
-    <Modal open={open} onClose={handleClose} closeAfterTransition sx={{
-      position: 'absolute',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
-      <Grow in={open}>
-        <Box sx={MODAL_STYLE}>
-          <img src={inputBooks.image} alt={inputBooks.title} style={{ paddingBottom: 16 }} />
-          <BaseModalParts inputBooks={inputBooks} setInputBooks={setInputBooks} />
-          <Box sx={{ display: 'flex', mt: 2, gap: 1 }}>
-            {modalMode === "edit" && <Button variant="contained" color="error" onClick={() => handleEdit("delete")}>削除</Button>}
-            <Box sx={{ flexGrow: 1 }} />
-            <Button variant="outlined" onClick={handleClose}>閉じる</Button>
-            {modalMode === "submit" && <Button variant="contained" onClick={handleSubmitCustom}>登録</Button>}
-            {modalMode === "edit" && <Button variant="contained" onClick={() => handleEdit("edit")}>完了</Button>}
-          </Box>
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={MODAL_STYLE}>
+        <img src={inputBooks.image} alt={inputBooks.title} style={{ paddingBottom: 16 }} />
+        <BaseModalParts inputBooks={inputBooks} setInputBooks={setInputBooks} />
+        <Box sx={{ display: 'flex', mt: 2, gap: 1 }}>
+          {modalMode === "edit" && <Button variant="contained" color="error" onClick={() => handleEdit("delete")}>削除</Button>}
+          <Box sx={{ flexGrow: 1 }} />
+          <Button variant="outlined" onClick={handleClose}>閉じる</Button>
+          {modalMode === "submit" && <Button variant="contained" onClick={handleSubmitCustom}>登録</Button>}
+          {modalMode === "edit" && <Button variant="contained" onClick={() => handleEdit("edit")}>完了</Button>}
         </Box>
-      </Grow>
-    </Modal>
+      </Box>
+    </Modal >
   );
 }
 
--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,10 +1,10 @@
 export const BASE_URL = '/BookManager';
 export const BOOK_SHADOW = 8;
 export const MODAL_STYLE = {
-  // position: 'absolute',
-  // top: '0%',
-  // left: '0%',
-  // transform: 'translate(-50%, -50%)',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   bgcolor: 'background.paper',
   borderRadius: 2,
   width: 280,
