5a79c69b0759f0139e660562ace74accc2351924
magumanakayama <domoido@icloud.com>
2025-11-29 11:20:14 +0900

    PWA元に戻す

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,34 +4,33 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [
-  //   react(),
-  //   VitePWA({
-  //     registerType: 'autoUpdate',
-  //     devOptions: {
-  //       enabled: true
-  //     },
-  //     manifest: {
-  //       name: 'Book Manager PWA',
-  //       short_name: 'Book Manager',
-  //       description: 'A PWA for managing your book collection',
-  //       theme_color: '#ffffff',
-  //       icons: [
-  //         {
-  //           src: 'assets/icon_192.png',
-  //           sizes: '192x192',
-  //           type: 'image/png'
-  //         },
-  //         {
-  //           src: 'assets/icon_512.png',
-  //           sizes: '512x512',
-  //           type: 'image/png'
-  //         }
-  //       ]
-  //     }
-  //   })
-  // ],
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Book Manager PWA',
+        short_name: 'Book Manager',
+        description: 'A PWA for managing your book collection',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/icon_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'assets/icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: '0.0.0.0',
     port: 3000
